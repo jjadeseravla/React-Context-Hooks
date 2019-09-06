63d0aa527f35e45d236f6e3a97e96f1700377c2e
jjadeseravla <jadealvares@hotmail.co.uk>
2019-09-06 11:37:53 +0100

    theme context changes from light to dark

--- context-app/src/components/BookList.js
+++ context-app/src/components/BookList.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
+import { ThemeContext } from '../contexts/ThemeContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isLightTheme, light, dark } = this.context;
+    const theme = isLightTheme ? light: dark;
     return (
-      <div className='book-list'>
+      <div className='book-list' style={{ color: theme.syntax, background: theme.bg }}>
         <ul>
-          <li>Becoming</li>
-          <li>Game of Thrones</li>
-          <li>100 años de soledad</li>
+          <li style={{ background: theme.ui }}>Becoming</li>
+          <li style={{ background: theme.ui }}>Game of Thrones</li>
+          <li style={{ background: theme.ui }}>100 años de soledad</li>
         </ul>
       </div>
     );
