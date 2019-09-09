0e6cace0c2dd3c6d77459f4bfdc674918b7240a2
jjadeseravla <jadealvares@hotmail.co.uk>
2019-09-09 14:00:47 +0100

    added a reducer to reduce all different functions which interact with the state into one single function

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,21 +1,12 @@
-import React, { createContext, useState } from 'react';
-import uuid from 'uuid/v1';
+import React, { createContext, useReducer } from 'react';
+import { bookReducer } from '../reducers/bookReducer';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-  const [books, setBooks] = useState([
-    {title: 'Becoming', author: 'Michelle Obama', id: 1},
-    {title: 'Love', author: 'fuck knows', id: 2}
-  ]);
-  const addBook = (title, author) => {
-    setBooks([...books, {title: title, author: author, id: uuid() }])
-  }
-  const removeBook = (id) => {
-    setBooks(books.filter(book => book.id !== id))
-  };
+  const [books, dispatch] = useReducer(bookReducer, []); 
   return (
-    <BookContext.Provider value={{books, addBook, removeBook}}>
+    <BookContext.Provider value={{books, dispatch}}>
       { props.children }
     </BookContext.Provider>
   )
